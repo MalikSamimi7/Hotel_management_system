4d8e7084f87b718b78151e2318b865788cba7e4b
Malik Samimi <samimi.malik7@gmail.com>
2022-10-11 18:53:52 +0430

    labels done in dahsboard

--- src/main/java/controllers/Dashboard.java
+++ src/main/java/controllers/Dashboard.java
@@ -2,6 +2,8 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -15,6 +17,7 @@ import java.util.ResourceBundle;
 
 public class Dashboard implements Initializable {
     public Label todaybooks;
+    public LineChart chart;
     @FXML
     private Label totalincome;
     @FXML
@@ -24,6 +27,7 @@ public class Dashboard implements Initializable {
     dbconnector connector;
     Connection connection;
     int daybooks=0;
+
 
     public void setTodaybook() {
 
@@ -53,6 +57,12 @@ public class Dashboard implements Initializable {
         String date="2022-10-02";
         int t_total=0;
         int total=0;
+        int day=1;
+        int weekamount=0;
+        int weekno=1;
+        XYChart.Series series=new XYChart.Series();
+        series.setName("Amount");
+
         connector=new dbconnector();
         connection=connector.getconnection();
         String sql="select * from customer";
@@ -64,11 +74,29 @@ public class Dashboard implements Initializable {
          {
              t_total=t_total+Integer.parseInt(rs.getString(5));
              daybooks++;
+
          }
          total=total+Integer.parseInt(rs.getString(5));
+
+            if(date-(rs.getString(6).to)==7)
+            {
+                weekamount=weekamount+Integer.parseInt(rs.getString(5));
+                day++;
+            }
+            else
+            {
+
+                series.getData().add(new XYChart.Data<>(Integer.toString(weekno),weekamount));
+
+                weekno++;
+                day=1;
+                weekamount=0;
+            }
         }
         todaybooks.setText(Integer.toString(daybooks));
         todayincome.setText(Integer.toString(t_total)+" af");
         totalincome.setText(Integer.toString(total)+" af");
+        chart.getData().add(series);
+        System.out.println(weekno+" "+weekamount);
     }
 }
